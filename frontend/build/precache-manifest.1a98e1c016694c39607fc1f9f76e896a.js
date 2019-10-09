427b9cea39bb1f397710dd8e95c906ed551f1ebe
guimartinsalmeida <guimartinsalmeida@gmail.com>
2019-10-09 15:15:05 -0500

    despues de el deploy

---
+++ frontend/build/precache-manifest.1a98e1c016694c39607fc1f9f76e896a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df58eec5289f6ddabd3dee893bdd7cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5f0b6c4afb4b0717ddd",
+    "url": "/static/css/2.653462cc.chunk.css"
+  },
+  {
+    "revision": "eea149cc521b779344da",
+    "url": "/static/css/main.518fc507.chunk.css"
+  },
+  {
+    "revision": "e5f0b6c4afb4b0717ddd",
+    "url": "/static/js/2.044f3e6d.chunk.js"
+  },
+  {
+    "revision": "eea149cc521b779344da",
+    "url": "/static/js/main.8127e205.chunk.js"
+  },
+  {
+    "revision": "5ad6dc52180fca3143ed",
+    "url": "/static/js/runtime-main.a9e8376a.js"
+  }
+]);
